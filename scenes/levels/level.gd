96f0ef6a8a4238a77b9499015f9763c972d0f62c
Benedikt Schnatterbeck <benediktms@hey.com>
2023-12-21 20:26:18 +0000

    Zoom on player when entering the house

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,9 +3,9 @@ extends Node2D
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
-func _on_gate_player_entered_gate(body: Node2D):
-	print("player has entered gate")
-	print(body)
+func _on_gate_player_entered_gate(_body: Node2D):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
 
 func _on_player_grenade(pos: Vector2, dir: Vector2):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
@@ -19,3 +19,12 @@ func _on_player_laser(pos: Vector2, dir: Vector2):
 	laser.rotation_degrees = rad_to_deg(dir.angle()) + 90
 	laser.direction = dir
 	$Projectiles.add_child(laser)
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	# tween.tween_property($Player, "modulate:a", 0, 1).from(0.5) # access properties on a node with `:`
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1).set_trans(Tween.TRANS_QUAD)
