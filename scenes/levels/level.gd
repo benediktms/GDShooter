81e49ab4975559cc904f767abdfb7b87f50c1feb
Benedikt Schnatterbeck <benediktms@hey.com>
2023-12-21 20:48:00 +0000

    Set up inherited scene

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,11 +1,8 @@
 extends Node2D
+class_name ParentLevel
 
 const laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 const grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
-
-func _on_gate_player_entered_gate(_body: Node2D):
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.5)
 
 func _on_player_grenade(pos: Vector2, dir: Vector2):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
@@ -19,12 +16,3 @@ func _on_player_laser(pos: Vector2, dir: Vector2):
 	laser.rotation_degrees = rad_to_deg(dir.angle()) + 90
 	laser.direction = dir
 	$Projectiles.add_child(laser)
-
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	# tween.tween_property($Player, "modulate:a", 0, 1).from(0.5) # access properties on a node with `:`
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1).set_trans(Tween.TRANS_QUAD)
---
+++ scenes/levels/outside.gd
@@ -0,0 +1,14 @@
+extends ParentLevel
+
+func _on_gate_player_entered_gate(_body: Node2D):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	# tween.tween_property($Player, "modulate:a", 0, 1).from(0.5) # access properties on a node with `:`
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1).set_trans(Tween.TRANS_QUAD)
