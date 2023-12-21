76c01baa90af351f4aea9f2aa4cd2bc1e208a165
Benedikt Schnatterbeck <benediktms@hey.com>
2023-12-21 20:40:12 +0000

    Add logic to despawn laser

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 var speed: int = 400
 
-func _process(delta):
+func _process(_delta):
 	var direction = Vector2.RIGHT
 
 	velocity = direction * speed
 
 	# move and slide already uses delta time
 	move_and_slide()
+
+func hit():
+	print("hit")
+	queue_free()
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -6,3 +6,13 @@ var direction: Vector2 = Vector2.UP
 
 func _process(delta):
 	position += direction * speed * delta
+	var timer = $DespawnTimer
+
+	if timer.time_left == 0:
+		queue_free()
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+
+	queue_free()
