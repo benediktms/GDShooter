7b5f670eaae134a565ea1ad7c42b1c084b42a83f
Benedikt Schnatterbeck <benediktms@hey.com>
2024-01-05 17:24:14 +0100

    Use PackedScene for transition

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,6 +1,8 @@
 extends ParentLevel
 
+@export var outside_scene: PackedScene = preload("res://scenes/levels/outside.tscn")
+
 func _on_exit_gate_area_body_entered(_body: Node2D):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	get_tree().change_scene_to_packed(outside_scene)
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,9 +1,11 @@
 extends ParentLevel
 
+@export var inside_scene: PackedScene = preload("res://scenes/levels/inside.tscn")
+
 func _on_gate_player_entered_gate(_body: Node2D):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	get_tree().change_scene_to_packed(inside_scene)
 
 func _on_house_player_exited():
 	var tween = get_tree().create_tween()
